0fbfc0ff37fa589ab58f4d53852db80f7e3e0e68
Natasha Hakun <hakun.natasha@gmail.com>
2016-12-18 21:06:21 -0500

    Update app name and formatting.

--- js/app.js
+++ js/app.js
@@ -1,25 +1,24 @@
 (function() {
 
-var myApp = angular.module('myApp', [
-  'ui.router',
-  'ngSanitize'
-  ]);
+	var myApp = angular.module('myApp', [
+		'ui.router',
+		'ngSanitize'
+	]);
 
-  myApp.config(function($stateProvider, $urlRouterProvider) {
+	myApp.config(function($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise("/home");
+		$urlRouterProvider.otherwise("/home");
 
-    $stateProvider
-    .state('home', {
-        url: "/home",
-        templateUrl: "views/home.html"
-      })
+		$stateProvider
+		.state('home', {
+		    url: "/home",
+		    templateUrl: "views/home.html"
+		})
 
-    .state('portal', {
-        url: "/portal",
-        templateUrl: "views/portal.html"
-      })
-  }); //end config
+		.state('portal', {
+		    url: "/portal",
+		    templateUrl: "views/portal.html"
+		})
+	}); 
 
-
-}()); //end wrapper function
+}()); 

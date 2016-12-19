a7fc9206cb05c8c9e4cdf3e9b7c745420e8f8519
Natasha Hakun <hakun.natasha@gmail.com>
2016-12-18 21:07:42 -0500

    Remove unnecessary styles and create sass directory.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
- var gulp = require('gulp');
-	gutil = require('gulp-util');
-	concat = require('gulp-concat');
-	compass = require('gulp-compass');
+ var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	concat = require('gulp-concat'),
+	compass = require('gulp-compass'),
 	connect = require('gulp-connect');
 
 	
 
 // All Sass files here.
-var sassSources = ['css/style.scss'];
+var sassSources = ['sass/style.scss'];
 
 // All js files that are to be concatenated:
 var jsSources = [
@@ -19,9 +19,8 @@ var htmlSources = [
 	'*.html'
 ];
 
-// Concatenate all the jsSources files into script.js. Browserify then adds jquery and
-// modernizr. Be sure that you have 'global.$ = require("jquery")' in the first .js file.
-// Using connect.reload() on the last line to signal the server when a change is made here.
+// Concatenate all the jsSources files into script.js. 
+// Use connect.reload() on the last line to signal the server when a change is made here.
 gulp.task('js', function(){
 	gulp.src(jsSources)
 		.pipe(concat('script.js'))
@@ -30,13 +29,11 @@ gulp.task('js', function(){
 });
 
 
-// This task isn't using a config.rb file. If you want to use one, there's one included.
-// Search for gulp-compass for an example of how to reference config.rb.
-// Using connect.reload() on the last line to signal the server when a change is made here.
+// Compass is a framework with built-in mixins. Using connect.reload() on the last line to signal the server when a change is made here.
 gulp.task('compass', function(){
 	gulp.src(sassSources)
 		.pipe(compass({
-			sass: 'css',
+			sass: 'sass',
 			image: 'img',
 			comments: true,
 			style: 'expanded'
@@ -50,7 +47,7 @@ gulp.task('compass', function(){
 // doesn't change. Have to actually watch *.scss
 gulp.task('watch', function() {
 	gulp.watch(jsSources, ['js']),
-	gulp.watch('css/*.scss', ['compass']),
+	gulp.watch('sass/*.scss', ['compass']),
 	gulp.watch(htmlSources, ['html'])
 });
 
